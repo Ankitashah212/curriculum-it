1a824bf554919a5058794592035289c5deea3ab2
Kita <ankitashah212@gmail.com>
2017-10-10 08:58:10 -0400

    s

--- config/connection.js
+++ config/connection.js
@@ -3,20 +3,21 @@ var mysql = require("mysql");
 
 var connection;
 
-if(process.env.JAWSDB_URL) {
+if (process.env.JAWSDB_URL) {
   //Heroku deployment
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "password", //put your password here
-  database: "curriculum_db"
-});
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: "password", //put your password here
+    database: "curriculum_db"
+  });
+}
 
 // Make connection.
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) {
     console.error("error connecting: " + err.stack);
     return;
--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/userController.js");
+var routes = require("./controllers/appController.js");
 
 app.use("/", routes);
 
