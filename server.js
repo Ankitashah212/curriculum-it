ff84c60c1823ecabc1c73aac9d8017dd476ed4c7
Kita <ankitashah212@gmail.com>
2017-10-10 08:38:41 -0400

    last point before branching

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/songController.js");
+var routes = require("./controllers/userController.js");
 
 app.use("/", routes);
 
