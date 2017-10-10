f32921df3ddda2d1deca0b41c7dfc7bf3e62b46a
Kita <ankitashah212@gmail.com>
2017-10-09 21:22:17 -0400

    adding basic DB structure and starter data seed

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,39 @@
-### Schema
+DROP DATABASE IF EXISTS curriculum_db;
+CREATE DATABASE curriculum_db;
+USE curriculum_db;
 
-CREATE IF NOT EXISTS DATABASE songs_db;
-USE songs_db;
-
-CREATE TABLE songs
-(
-	id int NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
-	listened BOOLEAN DEFAULT false,
-	PRIMARY KEY (id)
+CREATE TABLE users(
+  userid VARCHAR(50) NOT NULL,
+  name VARCHAR(50),
+  password VARCHAR(10),
+  PRIMARY KEY (userid)
 );
+
+CREATE TABLE cource(
+  courceid INTEGER(11) AUTO_INCREMENT NOT NULL,
+  name VARCHAR(50),
+  description VARCHAR(150),
+  PRIMARY KEY (courceid)
+);
+
+
+CREATE TABLE users_to_cource(
+  signups INTEGER(11) AUTO_INCREMENT NOT NULL,
+  userid VARCHAR(50),
+  courceid INTEGER(11),
+  inprogress BOOLEAN DEFAULT false,
+  PRIMARY KEY (signups),
+  CONSTRAINT fk_cource FOREIGN KEY (courceid) REFERENCES cource(courceid),
+  CONSTRAINT fk_users FOREIGN KEY (userid) REFERENCES users(userid)
+);
+
+
+
+CREATE TABLE steps(
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  name VARCHAR(50),
+  courceid INTEGER(11),
+  PRIMARY KEY (id),
+CONSTRAINT fk_id_cource FOREIGN KEY (courceid) REFERENCES cource(courceid)
+);
+
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,22 @@
-INSERT INTO cats (name) VALUES ('I don\'t want to miss a thing');
-INSERT INTO cats (name) VALUES ('Walk this way');
-INSERT INTO cats (name) VALUES ('Dream On');
+
+
+INSERT INTO users (userid, name, password ) values ('ankita', 'ankita', "ankita");
+INSERT INTO users (userid, name, password ) values ('tara', 'tara', "tara");
+INSERT INTO users (userid, name, password ) values ('jose', 'jose', "jpse");
+INSERT INTO users (userid, name, password ) values ('julia', 'julia', "julia");
+INSERT INTO users (userid, name, password ) values ('ciarra', 'ciarra', "ciarra");
+
+
+INSERT INTO cource (name, description) values ('html5','entry level cource');
+INSERT INTO cource (name, description) values ('C','fundamentals of programming');
+INSERT INTO cource (name, description) values ('javascript','Async BS');
+
+INSERT into users_to_cource (userid, courceid) values ('ankita', 2);
+
+INSERT into steps (name, courceid) values ('pointers in C', 2);
+INSERT into steps (name, courceid) values ('STD in and out in C', 2);
+INSERT into steps (name, courceid) values ('loops', 2);
+
+
+
+
