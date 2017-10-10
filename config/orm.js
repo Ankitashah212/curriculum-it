d0bd61e66c72624578b4c052016d62151a99f675
Tara <tara@nelsonwriting.com>
2017-10-10 11:29:27 -0400

    Added server connection to sequelize

--- config/orm.js
+++ config/orm.js
@@ -1,37 +1,82 @@
 const mysql2 = require("mysql2");
 const Sequelize = require('sequelize');
 const connection = new Sequelize('curriculum_db', 'root', 'root', {
-    dialect: 'mysql'
+  host: 'localhost',    
+  dialect: 'mysql',
+
+  pool: {
+      max: 5,
+      min: 0,
+      idle: 10000
+    },
+  
 });
 
 const User = connection.define('user', {
-    userid: {
-        type: Sequelize.STRING(50),
-        unique: true,
-        primaryKey: true
-    },
-    name: {
-        type: Sequelize.STRING(50),
-    },
-    password: {
-        type: Sequelize.STRING(15),
-        allowNull: false        
-    }
+  userid: {
+      type: Sequelize.STRING(50),
+      unique: true,
+      allowNull: false,
+      primaryKey: true
+  },
+  name: {
+      type: Sequelize.STRING(50),
+  },
+  password: {
+      type: Sequelize.STRING(15),
+      allowNull: false        
+  }
 });
+
+
+const Course = connection.define('course', {
+  courseid: {
+      type: Sequelize.INTEGER(11),
+      unique: true,
+      allowNull: false,
+      primaryKey: true, autoIncrement: true
+  },
+  name: {
+      type: Sequelize.STRING(50),
+  },
+  description: {
+      type: Sequelize.STRING(150),        
+  }
+});
+
+
 
 User.sync({force: true}).then( function() {
-    // Table created
-    return User.create({
-        userid: 'tara',
-        name: 'tara',
-        password: 'tara',
-    });
+  // Table created
+  return User.create({
+      userid: 'tara',
+      name: 'tara',
+      password: 'tara',
+  });
 });
 
-  connection.sync(User).then(function () {
+Course.sync({force: true}).then( function() {
+  // Table created
+  return Course.create({
+      name: 'html5',
+      description: 'entry level course'
+  });
+});
 
-    User.findAll().then(function(users) {
-        console.log(users.length);
-    });
+connection.sync(User).then(function () {
 
+  User.findAll().then(function(users) {
+      console.log(users.length);
+  });
+
+});
+
+
+  sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
   });
