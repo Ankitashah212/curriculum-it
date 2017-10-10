732f54081223048819faa8a10367d9e79c7185d0
Tara Nelson <tara@nelsonwriting.com>
2017-10-10 09:13:06 -0400

    Changed 'cource' to 'course' -- spell checked

--- db/schema.sql
+++ db/schema.sql
@@ -9,21 +9,21 @@ CREATE TABLE users(
   PRIMARY KEY (userid)
 );
 
-CREATE TABLE cource(
-  courceid INTEGER(11) AUTO_INCREMENT NOT NULL,
+CREATE TABLE course(
+  courseid INTEGER(11) AUTO_INCREMENT NOT NULL,
   name VARCHAR(50),
   description VARCHAR(150),
-  PRIMARY KEY (courceid)
+  PRIMARY KEY (courseid)
 );
 
 
-CREATE TABLE users_to_cource(
+CREATE TABLE users_to_course(
   signups INTEGER(11) AUTO_INCREMENT NOT NULL,
   userid VARCHAR(50),
-  courceid INTEGER(11),
+  courseid INTEGER(11),
   inprogress BOOLEAN DEFAULT false,
   PRIMARY KEY (signups),
-  CONSTRAINT fk_cource FOREIGN KEY (courceid) REFERENCES cource(courceid),
+  CONSTRAINT fk_course FOREIGN KEY (courseid) REFERENCES course(courseid),
   CONSTRAINT fk_users FOREIGN KEY (userid) REFERENCES users(userid)
 );
 
@@ -32,8 +32,8 @@ CREATE TABLE users_to_cource(
 CREATE TABLE steps(
   id INTEGER(11) AUTO_INCREMENT NOT NULL,
   name VARCHAR(50),
-  courceid INTEGER(11),
+  courseid INTEGER(11),
   PRIMARY KEY (id),
-CONSTRAINT fk_id_cource FOREIGN KEY (courceid) REFERENCES cource(courceid)
+CONSTRAINT fk_id_course FOREIGN KEY (courseid) REFERENCES course(courseid)
 );
 
