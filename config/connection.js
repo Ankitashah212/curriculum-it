85f699a34a442b01561106ca5d43ebeeb6110964
Tara <tara@nelsonwriting.com>
2017-10-11 09:18:46 -0400

    Merge conflict solved

--- config/connection.js
+++ config/connection.js
@@ -1,49 +1,3 @@
-<<<<<<< HEAD
-// Set up MySQL connection.
-require("Sequelize");
-
-var connection;
-
-const sequelize = new Sequelize('database', 'username', 'password', {
-  host: 'localhost',
-  dialect: 'mysql',
-
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
-
-  
-});
-
-
-/*
-if (process.env.JAWSDB_URL) {
-  //Heroku deployment
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    port: 3306,
-    host: "localhost",
-    user: "root",
-    password: "password", //put your password here
-    database: "curriculum_db"
-  });
-}
-*/
-// Make connection.
-connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
-// Export connection for our ORM to use.
-module.exports = connection;
-=======
 var Sequelize = require("sequelize");
 const mysql2 = require("mysql2");
 
@@ -66,4 +20,3 @@ sequelize
 });
 
 module.exports = sequelize;
->>>>>>> 4b07b1634f14c251a2714ebf6f9aacd7fad30cb9
