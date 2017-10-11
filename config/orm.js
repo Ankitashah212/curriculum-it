b29ab3fa61c42a5ceab5ace51b5738245fdf9f1d
Kita <ankitashah212@gmail.com>
2017-10-11 15:08:56 -0400

    added model

--- config/orm.js
+++ config/orm.js
@@ -3,9 +3,9 @@ var connection = require("../config/connection.js");
 
 // Object for all our SQL statement functions.
 var orm = {
-  myCourses: function (userid, cb) {
-    var queryString = "select c.name , c.description from users u, course c, users_to_course s where"
-      + "(u.userid = s.userid) and (c.courseid = s.courseid) and"
+  myCources: function (userid, cb) {
+    var queryString = "select c.name from users u, course c, users_to_course s where"
+      + "(u.userid = s.userid) and (c.courceid = s.courceid) and 
       + "(s.userid = '" + userid + "')";
     connection.query(queryString, function (err, result) {
       if (err) {
@@ -14,9 +14,9 @@ var orm = {
       cb(result);
     });
   },
-  addCourse: function (courseid, userid, cb) {
-    var queryString = "INSERT INTO users_to_course (userid, courseid) values ("
-    +"'" + userid +"', "+ courseid + ")";
+  addCource: function (courceid, userid, cb) {
+    var queryString = "INSERT INTO users_to_course (userid, courceid) values ("
+    +"'" + userid +"', "+ courceid + ")";
 
     console.log(queryString);
 
@@ -29,7 +29,7 @@ var orm = {
     });
   },
   
-  deleteCourse: function (id, cb) {
+  deleteCource: function (id, cb) {
     var queryString = "delete from users_to_course where signups = " + id;
   
     console.log(queryString);
@@ -43,7 +43,7 @@ var orm = {
     });
   },
   // An example of objColVals would be {name: panther, sleepy: true}
-  updateCourse: function (id, cb) {
+  updateCource: function (id, cb) {
     var queryString = " update users_to_course set inprogress = 1  where signups = " + id;
 
     console.log(queryString);
@@ -55,7 +55,7 @@ var orm = {
     });
   },
   // An example of objColVals would be {name: panther, sleepy: true}
-  allCourse: function ( cb) {
+  allCource: function ( cb) {
     var queryString = "select * from course ";
 
     console.log(queryString);
